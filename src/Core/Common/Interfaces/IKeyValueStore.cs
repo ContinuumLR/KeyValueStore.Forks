585a013a987d5f1becb187fb209ce80663c1c9a0
Royi Mindel <r.mindel@gmail.com>
2017-08-09 18:33:59 +0300

    IKeyValueStore, ForksWrapper

---
+++ src/Core/Common/Interfaces/IKeyValueStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KVS.Forks.Core.Interfaces
+{
+    public interface IKeyValueStore<TDataTypesEnum>
+    {
+        bool Set<T>(TDataTypesEnum type, string key, T value, object extraParams );
+        bool Set<T>(TDataTypesEnum type, IEnumerable<Tuple<string, T, object>> values);
+        T Get<T>(TDataTypesEnum type, string key, object extraParams);
+        IEnumerable<T> Get<T>(TDataTypesEnum type, IEnumerable<Tuple<string, object>> keys);
+
+        TDataTypesEnum DefaultType { get; }
+    }
+}
---
+++ src/Core/Forks/ForksWrapper.cs
@@ -0,0 +1,50 @@
+﻿using KVS.Forks.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace KVS.Forks.Core
+{
+    public class ForksWrapper<TDataTypesEnum>
+    {
+        public ForksWrapper(IKeyValueStore<TDataTypesEnum> keyValueStore)
+        {
+            _keyValueStore = keyValueStore ?? throw new ArgumentNullException(nameof(keyValueStore));
+        }
+
+        private IKeyValueStore<TDataTypesEnum> _keyValueStore;
+        public IKeyValueStore<TDataTypesEnum> KeyValueStore
+        {
+            get
+            {
+                return _keyValueStore;
+            }
+        }
+
+        // Forks logic will be contained here, checking versions
+        //
+        // Use case:
+        // var wrapper = new ForksWrapper<RedisDataTypesEnum>();
+        // wrapper.Set<User>(type.String, "key", user, null);
+        // wrapper.Set<UserInHash>(type.Hash, "key2", user, new RedisHashParams { HashKey = "hashKey" });
+
+        public bool Set<T>(TDataTypesEnum type, string key, T value, object extraParams)
+        {
+            return KeyValueStore.Set(type, key, value, extraParams);
+        }
+
+        public bool Set<T>(TDataTypesEnum type, IEnumerable<Tuple<string, T, object>> values)
+        {
+            return KeyValueStore.Set(type, values);
+        }
+
+        public T Get<T>(TDataTypesEnum type, string key, object extraParams)
+        {
+            return KeyValueStore.Get<T>(type, key,extraParams);
+        }
+
+        public IEnumerable<T> Get<T>(TDataTypesEnum type, IEnumerable<Tuple<string, object>> keys)
+        {
+            return KeyValueStore.Get<T>(type, keys);
+        }
+    }
+}
